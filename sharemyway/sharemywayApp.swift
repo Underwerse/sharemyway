30f56467c72c819bf34098107d92f977f119e262
pavelc <pavelc@metropolia.fi>
2022-11-13 17:37:33 +0200

    feat: create new project from scratch.
    Signed-off-by: pavelc <pavelc@metropolia.fi>

---
+++ sharemyway/sharemywayApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharemywayApp.swift
+//  sharemyway
+//
+//  Created by iosdev on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct sharemywayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
